ce8f5148e1d70839e5a7afb921458df0139507c8
blob1234567890 <ben.k.olson@gmail.com>
2025-06-30 16:39:39 -0700

    extra linking and parsers are unneeded
    linking already happens by home.file.".config" and parsers by just
    including the package

--- modules/home-manager/ben/home.nix
+++ modules/home-manager/ben/home.nix
@@ -55,20 +55,20 @@ in {
     programs.home-manager.enable = true; # let home-manager manager itself
 
     # https://github.com/nvim-treesitter/nvim-treesitter#i-get-query-error-invalid-node-type-at-position
-    xdg.configFile."nvim/parser".source =
-        let
-            parsers = pkgs.symlinkJoin {
-                name = "treesitter-parsers";
-                paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
-                    c
-                    lua
-                ])).dependencies;
-            };
-        in
-        "${parsers}/parser";
+    # xdg.configFile."nvim/parser".source =
+    #     let
+    #         parsers = pkgs.symlinkJoin {
+    #             name = "treesitter-parsers";
+    #             paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
+    #                 c
+    #                 lua
+    #             ])).dependencies;
+    #         };
+    #     in
+    #     "${parsers}/parser";
 
-    # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
-    xdg.configFile."nvim/lua".source = ./config/nvim/lua;
+    # # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
+    # xdg.configFile."nvim/lua".source = ./config/nvim/lua;
 
     imports = [
         # (import ./config/alacritty/default.nix { inherit pkgs; })
