69dbd12e0c739cb3b9df18f96dab5e9843d55c6c
blob1234567890 <ben.k.olson@gmail.com>
2025-06-30 17:10:03 -0700

    wezterm depends on 0xProto font

--- modules/home-manager/ben/config/wezterm/default.nix
+++ modules/home-manager/ben/config/wezterm/default.nix
@@ -1,6 +1,7 @@
 { pkgs, colors, ... }:
 
 with colors; {
+    home.packages = [ pkgs.nerd-fonts._0xproto ];
     programs.wezterm = {
         enable = true;
         colorSchemes = import ./colors.nix { inherit colors; };
