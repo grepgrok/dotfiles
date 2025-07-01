0d6b116380ceb8446cad4f418da176b27eaeb783
blob1234567890 <ben.k.olson@gmail.com>
2025-06-30 22:48:39 -0700

    handled pulling from github

--- config/wezterm/default.nix
+++ config/wezterm/default.nix
@@ -1,5 +1,4 @@
 { pkgs, colors, ... }:
-
 with colors; {
     home.packages = [ pkgs.nerd-fonts._0xproto ];
     programs.wezterm = {
--- modules/hosts/Borealis/settings/environment.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-    environment = {
-        shells = with pkgs; [ zsh bash ];
-        systemPackages = [ pkgs.coreutils ];
-        systemPath = [ "/usr/local/bin" ];
-        pathsToLink = [ "/Applications" ]; # symlinks /run/current-system/sw
-    };
-}
