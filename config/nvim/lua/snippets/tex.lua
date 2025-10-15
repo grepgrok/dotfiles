4f391d70c8e181808f7474f92d306e710843d4fc
blob1234567890 <ben.k.olson@gmail.com>
2025-10-15 12:35:56 +0900

    fine-tuning snippets

--- config/nvim/lua/snippets/tex.lua
+++ config/nvim/lua/snippets/tex.lua
@@ -8,7 +8,7 @@ local decorator = {
     wordTrig = false,
     condition = pipe({
         is_math,
-        --no_backslash
+        no_backslash,
     }),
     trigEngine = "vim",
     priority = 5000,
@@ -20,17 +20,18 @@ local t = ls.text_node
 
 -- First list: controlled snippets (show up as autocomplete options)
 -- Second list: autosnippets
-return {}, {
-    ls.snippet({ trig = "asdf", trigEngine = "vim" }, t("auto works")),
-
+return {
+    s({ trig = "@w", name = "omega" }, t("\\omega ")),
+}, {
     s(
-        { trig = "\\([NZQRSTKCF]\\)\\1", name = "math blackboard bold" },
+        -- \C explicitly tells vim to be case sensitive
+        { trig = "\\C\\([NZQRSTKCF]\\)\\1", name = "math blackboard bold", priority = 4000 },
         f(function(_, snip)
             return "\\mathbb{" .. snip.captures[1] .. "}"
         end, {})
     ),
     s(
-        { trig = "\\([P]\\)\\1", name = "math caligraphic" },
+        { trig = "\\C\\([P]\\)\\1", name = "math caligraphic" },
         f(function(_, snip)
             return "\\mathcal{" .. snip.captures[1] .. "}"
         end, {})
@@ -52,7 +53,14 @@ return {}, {
     s({ trig = "cap", name = "set intersection" }, t("\\cap ")),
     s({ trig = "eset", name = "emptyset" }, t("\\emptyset ")),
     s({ trig = "00", name = "emptyset" }, t("\\emptyset ")),
-    s({ trig = "\\.\\.\\.", name = "dots", priority = 5000 }, t("\\dots ")),
+    s(
+        { trig = "\\(\\%[,]\\)\\.\\.\\.", name = "dots" },
+        f(function(_, snip)
+            local prefix = snip.captures[1] == "," and ", " or ""
+            return prefix .. "\\dots "
+        end, {})
+    ),
+    s({ trig = "\\\\dots ,", name = "comma dots", condition = is_math }, t("\\dots,")),
 
     -- TODO: liminf -> \liminf NOT lim\inf
     --parse({ trig = "liminf", name = "liminf", priority = 0 }, "\\liminf_{${1:n} \\to ${2:\\infty}} "),
