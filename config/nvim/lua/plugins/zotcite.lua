27e67ce66a3b46d609684aafa3cc2f552cabcd86
blob1234567890 <ben.k.olson@gmail.com>
2026-01-05 23:52:29 +0900

    zotcite: seems to conflict with LuaSnip autosnippets

--- config/nvim/lua/plugins/zotcite.lua
+++ config/nvim/lua/plugins/zotcite.lua
@@ -1,9 +1,11 @@
 return {
     "jalvesaq/zotcite",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-telescope/telescope.nvim",
-    },
+    -- TODO: for some reason, zotcite breaks autosnippets
+    enabled = false,
+    --dependencies = {
+    --"nvim-treesitter/nvim-treesitter",
+    --"nvim-telescope/telescope.nvim",
+    --},
     -- TODO: Add menu title under [leader]
     config = function()
         require("zotcite").setup({
