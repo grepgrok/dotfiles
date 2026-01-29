82b73debfda06dc914f472c3b57a48da734e9981
blob1234567890 <ben.k.olson@gmail.com>
2026-01-29 22:45:29 +0900

    note(nvim): I really need to understand nvim + nix inside and out

--- config/nvim/default.nix
+++ config/nvim/default.nix
@@ -5,6 +5,19 @@
     ...
 }:
 let
+    # https://github.com/LazyVim/LazyVim/discussions/1972
+    # This creates ~/.config/nvim/parser/treesitter-parsers/${language}/parser
+    # xdg.configFile."nvim/parser".source =
+    # let
+    #   parsers = pkgs.symlinkJoin {
+    #     name = "treesitter-parsers";
+    #     paths = (pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
+    #       c
+    #       lua
+    #     ])).dependencies;
+    #   };
+    # in
+    # "${parsers}/parser";
     linterLanguages = packages: with packages; [
         c
         haskell
@@ -32,20 +45,28 @@ let
 in
 {
     home.packages = dependencies;
-    ## Get LazyVim to work with Nix and home-manager
-    # https://github.com/LazyVim/LazyVim/discussions/1972#discussion-5826338
+
     programs.neovim = {
+        # TODO: enable should control the WHOLE nvim config, that includes porting the files to .config
         enable = true;
         # package = stable.neovim-unwrapped;
+
+        # Packages available to nvim
+        # i.e. can require()
         extraPackages = with pkgs; [
+            # LazyVim
             lua-language-server
             stylua
-            ripgrep
-            imagemagick # image.nvim
-        ];
 
-        extraLuaPackages = ps: with ps; [
-            ps.magick # image.nvim
+            # Telescope
+            ripgrep
+
+            # image.nvim
+            imagemagick
+        ];
+        extraLuaPackages = p: with p; [
+            # image.nvim
+            magick
         ];
 
         plugins = with pkgs.vimPlugins; [
